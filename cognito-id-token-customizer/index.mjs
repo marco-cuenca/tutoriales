a8598f936e9246e48633052df7b56c8dc2147953
mcuenca <marc_tony22@outlook.com>
2024-10-31 11:09:56 -0500

    chore(init): inicializacion de rama

--- cognito-id-token-customizer/index.mjs
+++ cognito-id-token-customizer/index.mjs
@@ -4,8 +4,8 @@ export const handler = async (event) => {
   const userAttributes = event.request.userAttributes;
 
   const data = new URLSearchParams({
-    client_id: "9b8b93cb-3e18-44ef-a041-12bdd0c8b92d",
-    client_secret: "aj28Q~KK6gQq2IzLv2_Rwf5Tz2J6_LpuRfIxmbM6",
+    client_id: "client_id_app_entraid",
+    client_secret: "client_secret_app_entraid",
     scope: "https://graph.microsoft.com/.default",
     grant_type: "client_credentials"
   });
@@ -16,7 +16,7 @@ export const handler = async (event) => {
     }
   };
 
-  const login = await axios.post("https://login.microsoftonline.com/be99e360-0df5-4c08-981b-f90011363e16/oauth2/v2.0/token", data, configLogin);
+  const login = await axios.post("https://login.microsoftonline.com/aca-va-el-id-del-tenant-del-app-entraid/oauth2/v2.0/token", data, configLogin);
 
   const configGetData = {
     headers: {
